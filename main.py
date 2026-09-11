eadf851968e4a4b01f76ffc162952c43fcc9a9e2
Nutsa <21100278@ibsu.edu.ge>
2026-09-11 23:46:15 +0400

    changes

--- main.py
+++ main.py
@@ -1,23 +1,94 @@
 import character_generator
-
+import random, string
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi import WebSocket, WebSocketDisconnect
+
 
 app = FastAPI()
+rooms ={}
+connections = {}
 
 @app.get("/")
 def home():
     return FileResponse("index.html")
 
-@app.get("/game")
-def game():
-    return FileResponse("game-page.html")
-
 @app.get("/script.js")
 def javascript():
     return FileResponse("script.js")
 
+
+def create_room():
+    room_id = ''.join(
+        random.choices(string.ascii_uppercase + string.digits, k=5)
+    )
+
+    rooms[room_id] = {
+        "players": []
+    }
+
+    return room_id
+
+
+@app.get("/join/{room_id}")
+def join_room(room_id: str):
+    if room_id not in rooms:
+        return {"error": "Room does not exist"}
+
+    rooms[room_id]["players"].append("player")
+
+    return {
+        "room_id": room_id,
+        "players": len(rooms[room_id]["players"])
+    }
+
+@app.get("/create-game")
+def create_game():
+    room_id = create_room()
+    return {"room_id": room_id}
+
+
+
+@app.get("/game/{room_id}")
+def game(room_id: str):
+    if room_id not in rooms:
+        return {"error": "Room does not exist"}
+
+    return FileResponse("game-page.html")
+
+
+
+@app.websocket("/ws/{room_id}")
+async def websocket_endpoint(websocket: WebSocket, room_id: str):
+    await websocket.accept()
+    player_id = ''.join(
+        random.choices(string.ascii_uppercase + string.digits, k=5)
+    )
+    rooms[room_id]["players"].append(player_id)
+    await websocket.send_text(player_id)
+
+    print("Player connected:", player_id)
+    if room_id not in connections:
+        connections[room_id] = []
+
+    connections[room_id].append(websocket)
+    for connection in connections[room_id]:
+        await connection.send_text(
+            str(rooms[room_id]["players"])
+        )
+    print("Player connected to room:", room_id)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            for connection in connections[room_id]:
+                await connection.send_text(data)
+
+    except WebSocketDisconnect:
+        connections[room_id].remove(websocket)
+        rooms[room_id]["players"].remove(player_id)
+        
 @app.get("/style.css")
 def css():
     return FileResponse("style.css")
--- script.js
+++ script.js
@@ -1,19 +1,74 @@
 
 console.log("HI");
+const playerNumberInput = document.getElementById("input-players");
 
-// const button1 = document.getElementById("button1");
+const inputValue =5;
+const beginButton = document.getElementById("begin-button")
+if (beginButton){
+document.getElementById("begin-button").addEventListener("click", async function() {
+    const response = await fetch("/create-game");
+    const data = await response.json();
 
-// // button1.addEventListener("click", async function() {
-// //     console.log("Button was clicked!");
-// //     const response = await fetch("/random-character");
-// //     const data = await response.json();
+    window.location.href = "/game/" + data.room_id;
+});
+};
+const button1 = document.getElementById("button1");
 
 
-// //     const img = document.createElement('img');
+if(button1){
 
-// //     img.src = data.image;
-// //     img.className = 'character-img'
-// //     img.alt = 'A descriptive text for accessibility';
-// //     const img_container = document.querySelector(".img-container")
-// //     img_container.append(img);
-// // });
+button1.addEventListener("click", async function() {
+    console.log("Button was clicked!");
+    console.log(inputValue)
+    socket.send("Hello from this player!");
+    socket.send("button1_clicked");
+
+    
+    for (let i =0;i <inputValue;i++ ){
+        console.log(i)
+        const response = await fetch("/random-character");
+        const data = await response.json();
+
+
+        const img = document.createElement('img');
+
+        img.src = data.image;
+        img.className = 'character-img'
+        img.alt = 'A descriptive text for accessibility';
+        const img_container = document.querySelector(".img-container")
+        img_container.append(img);
+    };
+
+
+});
+
+};
+
+const roomId = window.location.pathname.split("/").pop();
+
+async function joinRoom() {
+    const response = await fetch("/join/" + roomId);
+    const data = await response.json();
+
+    console.log("Room ID:", roomId);
+    console.log(data);
+}
+
+
+
+// joinRoom();
+
+const socket = new WebSocket(
+    `ws://${window.location.host}/ws/${roomId}`
+);
+
+socket.onopen = function() {
+    console.log("Connected to room!");
+};
+
+socket.onmessage = function(event) {
+    console.log("My player ID:", event.data);
+    if (event.data === "button1_clicked") {
+        button1.style.backgroundColor = "red";
+    }
+};
