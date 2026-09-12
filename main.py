446ec3a126c44a3f79b968856908655e3ae49da1
Nutsa <21100278@ibsu.edu.ge>
2026-09-12 18:30:43 +0400

    test ddg

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ import os
 import cv2
 import numpy as np
 from fastapi.responses import FileResponse, Response
-print("CV2 LOCATION:", cv2.__file__)
-print("CV2 VERSION:", cv2.__version__)
-print("HAS CASCADE:", hasattr(cv2, "CascadeClassifier"))
+import importlib.metadata
 
+print(importlib.metadata.version("ddgs"))
 app = FastAPI()
 rooms ={}
 connections = {}
